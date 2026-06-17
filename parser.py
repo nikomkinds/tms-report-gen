2da20012bae6cff7366cc5331aea0bd13ea2e94f
DmitriyNikomkin <134718130+str1xoid@users.noreply.github.com>
2026-06-17 14:40:22 +0300

    Implemented parser: CSV -> TestCase list

---
+++ parser.py
@@ -0,0 +1,70 @@
+import csv
+
+from models import TestCase, TestStep
+
+
+def str_to_bool(value: str) -> bool:
+    return value.lower() == "true"
+
+
+def load_test_cases(
+    cases_csv_path: str,
+    steps_csv_path: str,
+) -> list[TestCase]:
+
+    test_cases = {}
+
+    # Loading test cases from cases CSV file
+    with open(cases_csv_path, newline="", encoding="utf-8") as file:
+
+        reader = csv.DictReader(file)
+
+        for row in reader:
+
+            testcase = TestCase(
+                id=int(row["id"]),
+
+                name=row["name"],
+                title=row["title"],
+
+                status=int(row["status"]),
+
+                is_tracked=str_to_bool(row["is_tracked"]),
+                is_auto=str_to_bool(row["is_auto"]),
+                is_individ=str_to_bool(row["is_individ"]),
+                is_unique=str_to_bool(row["is_unique"]),
+
+                priority=int(row["priority"]),
+                test_type=int(row["type"]),
+                complexity=int(row["complexity"]),
+
+                steps=[]
+            )
+
+            test_cases[testcase.id] = testcase
+
+    # Loading test steps from steps CSV file
+    with open(steps_csv_path, newline="", encoding="utf-8") as file:
+
+        reader = csv.DictReader(file)
+
+        for row in reader:
+
+            step = TestStep(
+                id=int(row["id"]),
+                step_type=row["step_type"],
+                action=row["action"],
+                expected_result=row["expected_result"],
+                order=int(row["order"]),
+            )
+
+            case_id = int(row["case_id"])
+
+            if case_id in test_cases:
+                test_cases[case_id].steps.append(step)
+
+    # Sorting steps for each case
+    for testcase in test_cases.values():
+        testcase.steps.sort(key=lambda step: step.order)
+
+    return list(test_cases.values())
